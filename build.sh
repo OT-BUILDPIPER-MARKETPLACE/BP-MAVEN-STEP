3b275ca2d32f00b835deced76a39a4f515903ff0
Sandeep <sandeep@opstree.com>
2022-07-13 16:49:31 +0530

    Added Dockerfile & build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source BP-BASE-SHELL-STEPS/functions.sh
+
+echo "I'll build the code"
+sleep  $SLEEP_DURATION
+
+cd  $CODEBASE_DIR
+mvn $INSTRUCTION
+if [ $? -eq 0 ]
+then
+  generateOutput mvn_execute true "dhek chal gya"
+  echo "build sucessfull"
+elif  [ $? != 0 ]
+then 
+  generateOutput mvn_execute false "nhi chala"
+  echo "build unsucessfull"
+fi
